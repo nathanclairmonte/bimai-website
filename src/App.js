863da42faac15f7cf5070990617706f3204e5079
nathanclairmonte <nathanclairmonte@outlook.com>
2021-10-20 14:24:23 -0400

    slatt

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing deploying code changes through Git
+          slatt
         </a>
       </header>
     </div>
